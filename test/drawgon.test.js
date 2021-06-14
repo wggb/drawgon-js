723ce41087433fe5f30e8b6b2ab323f845e4166c
Sadra Saderi <sadrassx@gmail.com>
2021-06-14 21:17:25 +0430

    Fix initial test

--- test/drawgon.test.js
+++ test/drawgon.test.js
@@ -1,14 +1,35 @@
-const { Drawgon, DrawgonTool } = require('../src/drawgon');
+const rewire = require('rewire');
 
-test('initial test', function () {
+const script = rewire('../src/drawgon');
+
+// Mocks
+script.__set__("paper", {
+    install: function () { return; },
+    setup: function () { return; }
+});
+script.__set__("window", "");
+var querySelectorM = function () {
+    this.addEventListener = function () { return; };
+};
+script.__set__("document", {
+    querySelector: function () { return new querySelectorM; }
+});
+script.__set__("view", { zoom: 0, center: { x: 0, y: 0 } });
+script.__set__("Tool", function () { return; });
+
+var Drawgon = script.__get__("Drawgon");
+var DrawgonTool = script.__get__("DrawgonTool");
+
+// Tests
+test('Initial test', function () {
     let draw;
     expect(function () {
-        draw = Drawgon;
-        draw = DrawgonTool;
+        draw = new Drawgon();
+        draw = new DrawgonTool();
+        isNaN(draw);
     }).not.toThrow();
 
     expect(function () {
-        draw = drawgonTools;
-        isNaN(draw);
+        script.__get__("drawgonTools");
     }).toThrow();
 });
